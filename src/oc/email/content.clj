81535efa59db0b9f54981141398d5c555fa9e0b9
Sean Johnson <sean@snootymonkey.com>
2016-07-21 09:26:31 -0400

    Some cleanup to the CSS.

--- src/oc/email/content.clj
+++ src/oc/email/content.clj
@@ -271,7 +271,7 @@
 
 (defn html [snapshot]
   (str
-    "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
+    "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
     (h/html (head (keywordize-keys snapshot)))))
 
 (comment
