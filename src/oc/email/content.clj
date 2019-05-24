fe6f129015fdce06f3ca102780784803c1d0909c
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-05-24 15:42:31 -0400

    Modify email content to include an entry's abstract if it is present

--- src/oc/email/content.clj
+++ src/oc/email/content.clj
@@ -313,7 +313,8 @@
         avatar-url (user-avatar/fix-avatar-url config/filestack-api-key (:avatar-url publisher))
         headline (post-headline entry)
         vid (:video-id entry)
-        cleaned-body (text/truncated-body (:body entry))
+        cleaned-body (or (:abstract entry)
+                         (text/truncated-body (:body entry)))
         has-body (seq cleaned-body)]
     [:table
       {:cellpadding "0"
@@ -370,7 +371,8 @@
   (let [publisher (:publisher entry)
         avatar-url (user-avatar/fix-avatar-url config/filestack-api-key (:avatar-url publisher))
         vid (:video-id entry)
-        cleaned-body (text/truncated-body (:body entry))
+        cleaned-body (or (:abstract entry)
+                         (text/truncated-body (:body entry)))
         has-body (seq cleaned-body)
         published-date (time-format/unparse date-format-no-dot (time-format/parse iso-format (:published-at entry)))
         superuser-token (auth/user-token {:user-id (:user-id user)} config/auth-server-url config/passphrase "Email")]
