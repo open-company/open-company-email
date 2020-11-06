84a180305149909f2b5c155e9c2ddb0701ad990b
bago2k4 <iacopo@rc0.eu>
2020-11-06 17:56:11 +0100

    Update deps.

--- project.clj
+++ project.clj
@@ -14,20 +14,20 @@
   ;; All profile dependencies
   :dependencies [
     ;; Lisp on the JVM http://clojure.org/documentation
-    [org.clojure/clojure "1.10.1"]
+    [org.clojure/clojure "1.10.2-alpha4"]
     ;; HTML rendering https://github.com/weavejester/hiccup
     [hiccup "2.0.0-alpha2"]
     ;; Async programming tools https://github.com/ztellman/manifold
-    [manifold "0.1.9-alpha3"]
+    [manifold "0.1.9-alpha4"]
     ;; Namespace management https://github.com/clojure/tools.namespace
     ;; NB: org.clojure/tools.reader is pulled in by oc.lib
-    [org.clojure/tools.namespace "0.3.1" :exclusions [org.clojure/tools.reader]] 
+    [org.clojure/tools.namespace "1.0.0" :exclusions [org.clojure/tools.reader]] 
                  
     ;; General data-binding functionality for Jackson: works on core streaming API https://github.com/FasterXML/jackson-databind
-    [com.fasterxml.jackson.core/jackson-databind "2.11.2"]
+    [com.fasterxml.jackson.core/jackson-databind "2.12.0-rc1"]
 
     ;; Library for OC projects https://github.com/open-company/open-company-lib
-    [open-company/lib "0.17.29-alpha45"]
+    [open-company/lib "0.17.29-alpha54" :exclusions [org.clojure/tools.logging]]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; Component - Component Lifecycle https://github.com/stuartsierra/component
     ;; Schema - Data validation https://github.com/Prismatic/schema
@@ -43,7 +43,7 @@
 
   ;; All profile plugins
   :plugins [
-    [lein-environ "1.1.0"] ; Get environment settings from different sources https://github.com/weavejester/environ
+    [lein-environ "1.2.0"] ; Get environment settings from different sources https://github.com/weavejester/environ
   ]
 
   :profiles {
@@ -55,7 +55,7 @@
       }
       :plugins [
         ;; Linter https://github.com/jonase/eastwood
-        [jonase/eastwood "0.3.6"]
+        [jonase/eastwood "0.3.11"]
         ;; Static code search for non-idiomatic code https://github.com/jonase/kibit
         [lein-kibit "0.1.8" :exclusions [org.clojure/clojure]]
       ]
@@ -86,7 +86,7 @@
         ;; Runs bikeshed, kibit and eastwood https://github.com/itang/lein-checkall
         [lein-checkall "0.1.1"]
         ;; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
-        [lein-pprint "1.2.0"]
+        [lein-pprint "1.3.2"]
         ;; Check for outdated dependencies https://github.com/xsc/lein-ancient
         [lein-ancient "0.6.15"]
         ;; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
