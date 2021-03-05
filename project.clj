39f06d62ded59a9fabeda989c8a24b8832ba66f1
bago2k4 <iacopo@rc0.eu>
2021-03-05 13:59:14 +0100

    Bump lib.

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
     ;; ***************** (JWT schema changes, more info here: *****************
     ;; ******* https://github.com/open-company/open-company-lib/pull/82) ******
     ;; ************************************************************************
-    [open-company/lib "0.17.34-alpha8" :exclusions [org.clojure/tools.logging]]
+    [open-company/lib "0.17.34-alpha9" :exclusions [org.clojure/tools.logging]]
     ;; ************************************************************************
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; Component - Component Lifecycle https://github.com/stuartsierra/component
