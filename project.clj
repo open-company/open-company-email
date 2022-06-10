4e2ba172456fec7b83e899d144218c3ad98df2b2
bago2k4 <iacopo@rc0.eu>
2022-06-10 20:23:40 +0200

    Bump lib to latest version.

--- project.clj
+++ project.clj
@@ -42,7 +42,7 @@
     ;; ***************** (JWT schema changes, more info here: *****************
     ;; ******* https://github.com/open-company/open-company-lib/pull/82) ******
     ;; ************************************************************************
-    [open-company/lib "0.20.0-alpha"]
+    [open-company/lib "0.20.2-alpha1"]
     ;; ************************************************************************
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; Component - Component Lifecycle https://github.com/stuartsierra/component
